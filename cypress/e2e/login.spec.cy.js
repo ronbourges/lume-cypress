b945a4587b82527073701182eb957af60276885a
ronbourges <bourgesron@mail.com>
2025-06-29 22:27:49 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -13,7 +13,7 @@ describe('Orange HRM test', () => {
 
 
   it('login-success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.userNameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type (userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
@@ -21,7 +21,7 @@ describe('Orange HRM test', () => {
     cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
   })
      it('login-fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.userNameField).type (userData.userFail.username)
     cy.get(selectorsList.passwordField).type (userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
