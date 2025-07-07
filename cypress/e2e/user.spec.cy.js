68f80f8a63597fb00096a08ef279ed95c354d9e5
ronbourges <bourgesron@mail.com>
2025-07-06 21:40:52 -0300

    User update scenario added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -6,19 +6,23 @@ describe('Orange HRM test', () => {
     passwordField: "[name='password']",
     loginButton:  "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    wrondCredentialAlert: "[role='alert']"
+    wrondCredentialAlert: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']"
 
 
   }
 
 
-  it('login-success', () => {
+  it.only('User Info Update -success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.userNameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type (userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') 
     cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorsList.myInfoButton).click()
   })
      it('login-fail', () => {
     cy.visit('/auth/login')
